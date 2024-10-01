6e5dff03e7de7dd9cd9135f40821983318f5e891
mohamed laadjal <laadjalmohamed@gmail.com>
2024-10-01 13:47:59 +0200

    User registration/authentication

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
